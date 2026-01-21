110bbab5773430f71ddbea2a7cd2bee234e35507
ALDASTFRANCIS <aldasfrancis0@gmail.com>
2026-01-21 20:59:33 +0530

    text_summarization

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "summarize") {
 
-        fetch("http://127.0.0.1:8000/summarize", {
+        fetch("http://100.0.0.0:8000/summarize", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ url: request.url })
